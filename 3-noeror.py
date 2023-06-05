e5c523ffdd93cb0f9b1b44adfcd394dcd8386d6f
sem7655 <127785162+sem7655@users.noreply.github.com>
2023-06-05 21:49:08 +0300

    Create 3-noeror.py

---
+++ 3-noeror.py
@@ -0,0 +1,14 @@
+class StringVar:
+    def __init__(self, string=""):
+        self.string = string
+
+        def set(self, new_string):
+            self.string = new_string
+
+        def get(self):
+            return self.string
+
+        string_var = StringVar ('World!')
+        print (string_var.get ())
+        string_var.set ('Goodbye')
+        print (string_var.get ())
