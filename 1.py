c0fc52481ad5b1228fb33db7188b7144981c3a59
sem7655 <127785162+sem7655@users.noreply.github.com>
2023-06-06 10:12:22 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,14 +1,14 @@
 class StringVar:
-    def init(self):
-        self.string = ""
+    def __init__(self, string=""):
+        self.string = string
 
-        def set(self, new_string):
-            self.string = new_string
+    def set(self, new_string):
+        self.string = new_string
 
-        def get(self):
-            return self.string
+    def get(self):
+        return self.string
 
-        string_var = StringVar (' World!')
+        string_var = StringVar ('World!')
         print (string_var.get ())
         string_var.set ('Goodbye')
         print (string_var.get ())
